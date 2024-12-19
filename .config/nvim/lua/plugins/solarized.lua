2f61581d4eca5a1205203349af1b0c7e21e22ea5
Dan Berglund <dan@berglund.io>
2024-12-19 15:35:39 +0100

    Use a different solarized plugin

--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -1,8 +1,13 @@
 return {
-    {
-        "shaunsingh/solarized.nvim",
-        config = function()
-            require('solarized').set()
-        end,
-    }
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {},
+    config = function(_, opts)
+        vim.o.termguicolors = true
+        vim.o.background = 'light'
+        require('solarized').setup(opts)
+        vim.cmd.colorscheme 'solarized'
+    end,
 }
