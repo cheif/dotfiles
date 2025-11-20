c54ad9a2653ba1de1674d27981afb00ae74cb470
Dan Berglund <dan@berglund.io>
2025-11-20 14:12:10 +0100

    Use vertical telescope layout

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,11 @@ return {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            require('telescope').setup({
+                defaults = {
+                    layout_strategy = 'vertical'
+                }
+            })
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
