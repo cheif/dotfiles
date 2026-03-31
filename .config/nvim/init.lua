ab9f158be084993d86649ba8b0919c1f050b326b
Dan Berglund <dan@berglund.io>
2026-03-31 08:34:15 +0200

    Move solarized to `vim.pack`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
+vim.pack.add({ 'https://github.com/maxmx03/solarized.nvim' })
+require('solarized').setup()
+vim.o.termguicolors = true
+vim.o.background = 'light'
+vim.cmd.colorscheme 'solarized'
 vim.schedule(function()
 	require("config.lazy")
 end)
