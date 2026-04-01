15d2df16e6953ca61a69ac7d9ad5a6801f46fa94
Dan Berglund <dan@berglund.io>
2026-04-01 13:42:51 +0200

    Slightly better java integration

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -74,6 +74,17 @@ require('mini.completion').setup()
 
 
 -- Java
--- vim.pack.add({ 'https://github.com/nvim-java/nvim-java' })
--- require('java').setup()
--- vim.lsp.enable('jdtls')
+vim.pack.add({
+    {
+        src = 'https://github.com/JavaHello/spring-boot.nvim',
+        version = '218c0c26c14d99feca778e4d13f5ec3e8b1b60f0',
+    },
+    'https://github.com/MunifTanjim/nui.nvim',
+    'https://github.com/nvim-java/nvim-java'
+})
+require('java').setup({
+    java_debug_adapter = {
+        enable = false
+    }
+})
+vim.lsp.enable('jdtls')
