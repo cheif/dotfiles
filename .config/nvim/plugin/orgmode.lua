cd3c0c7c720025334ad2c95bed5a7b6b8f74257f
Dan Berglund <dan@berglund.io>
2026-04-01 16:19:04 +0200

    Move orgmode to `vim.plug`

---
+++ .config/nvim/plugin/orgmode.lua
@@ -0,0 +1,20 @@
+vim.pack.add({ 'https://github.com/nvim-orgmode/orgmode' })
+require('orgmode').setup({
+    org_agenda_files = {'~/org/*'},
+    org_default_notes_file = '~/org/refile.org',
+    org_log_into_drawer = 'LOGBOOK',
+    org_capture_templates = {
+        t = {
+            description = 'Task',
+            template = '* TODO %?\n  %u'
+        },
+        l = {
+            description = '[Maker]log',
+            template = '* %?',
+            target = '~/org/Makerlog.org',
+            datetree = true,
+        },
+    }
+})
+vim.lsp.enable('org')
+
